2f3a373d59345e400ffdf2c2f06e9e6c048a9447
bglee <deptno@gmail.com>
2018-11-09 19:15:38 +0900

    run main

--- lib/index.ts
+++ lib/index.ts
@@ -5,12 +5,12 @@ import {registerStreams} from './register/stream'
 const app = express()
 const port = process.env.PORT || 3000
 
-async function main() {
+!async function main() {
   const methods = ['post', 'get', 'put', 'patch', 'delete']
   const streams = ['ddb']
 
   registerMethods(app, methods)
-  registerStreams(1000, streams)
+  registerStreams(5000, streams)
 
   app.listen(port, () => console.log(`:mushroom: offline-gateway Ready to work ${port}`))
-}
+}()
